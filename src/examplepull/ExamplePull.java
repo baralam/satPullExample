3ebc55eab78784cf4553b69d3e16616aa48cdd6d
baralam <66073700+baralam@users.noreply.github.com>
2020-06-20 22:27:46 -0400

    Update ExamplePull.java

--- src/examplepull/ExamplePull.java
+++ src/examplepull/ExamplePull.java
@@ -15,10 +15,11 @@ public class ExamplePull {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Leraning pull operations in github ");
-                System.out.println("Learning another pull  and fetch operations in github ");
-                               System.out.println("Learning another pull  and fetch 1234 operations in github ");
-                                             System.out.println("Learning another pull  and fetch 5678 operations in github ");
+    System.out.println("Leraning pull operations in github ");
+    System.out.println("Learning another pull  and fetch operations in github ");
+    System.out.println("Learning another pull  and fetch 1234 operations in github ");
+    System.out.println("Learning another pull  and fetch 5678 operations in github ");
+    System.out.println("LEARNING another pull practice ");
 
 
         
