8a521ac3a47e05f5d2249f08e952de836d5d1f97
baralam <baralam@sheridancollege.ca>
2020-06-20 22:01:11 -0400

    first commit

---
+++ src/examplepull/ExamplePull.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examplepull;
+
+/**
+ *
+ * @author baral
+ */
+public class ExamplePull {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Leraning pull operations in github ");
+        
+    }
+    
+}
